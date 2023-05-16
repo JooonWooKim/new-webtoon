456f50e9f71fdb185089c3aa9c4fd0097e78c87f
JoonwooKim <kkn8010@gmail.com>
2023-05-16 22:01:41 +0900

    feat: Enum 한글에서 영어로 변경

--- src/main/java/com/naver/webtoon/domain/webtoon/enums/SerializedStatus.java
+++ src/main/java/com/naver/webtoon/domain/webtoon/enums/SerializedStatus.java
@@ -1,5 +1,20 @@
 package com.naver.webtoon.domain.webtoon.enums;
 
+import com.naver.webtoon.global.common.exception.WebtoonException;
+
+import static com.naver.webtoon.global.common.exception.ErrorCode.NOT_FOUND_SERIALIZED_STATUS;
+
 public enum SerializedStatus {
-    SERIALIZED, PAUSE, COMPLETE
+    SERIALIZED,
+    PAUSE,
+    COMPLETE;
+
+    public static SerializedStatus toSerializedStatus(String serializedStatus){
+        return switch(serializedStatus){
+            case "SERIALIZED" -> SERIALIZED;
+            case "PAUSE" -> PAUSE;
+            case "COMPLETE" -> COMPLETE;
+            default -> throw new WebtoonException(NOT_FOUND_SERIALIZED_STATUS);
+        };
+    }
 }
